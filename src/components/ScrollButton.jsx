0bd4dec765ceb35a5f2830cdcc1949bd2c025347
*Marcos <maromasi@hotmail.com>
2023-03-08 17:43:39 -0600

    third commit

--- src/components/ScrollButton.jsx
+++ src/components/ScrollButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ScrollButton = () => {
   return (
     <div>
-        <a href="home">
+        <a href="#home">
             <button className='fixed bottom-10 right-4 p-2 bg-[#00c2ff] text-white rounded-full shadow-lg sm:bottom-11 sm:right-8'>
                 <i className='bx bx-up-arrow-alt w-7 h-7 text-xl'></i>
             </button>
